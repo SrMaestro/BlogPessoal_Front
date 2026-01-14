dce0bb4a8c769bdb03ed8bee7daffc06b049abae
Luis Felipe Ramires Leão <luisramires29032002@gmail.com>
2026-01-14 13:56:00 -0300

    feat: Atualizando App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar"
 import Home from "./Pages/home/Home"
 
 
 function App() {
 	return (
 		<>
+			<Navbar/>
 			<Home />
+			<Footer/>
+
 		</>
 	)
 }
